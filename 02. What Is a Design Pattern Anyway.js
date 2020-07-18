cba6c8d82ab53be7416c3d2e344c2c9d344260ef
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-18 11:57:01 +0300

    02. What Is a Design Pattern Anyway is finished

--- 02. What Is a Design Pattern Anyway.js
+++ 02. What Is a Design Pattern Anyway.js
@@ -1,2 +1,115 @@
 /* What Is a Design Pattern Anyway
 
+Design Patterns:
+
+> Who created them?
+> What are they?
+> When to use them?
+> Where to use them?
+> How to implement them?
+
+~ ~ ~ ~ ~
+
+JS
+“It was the Rodney Dangerfield of languages”
+Brendan Eich
+
+~ ~ ~ ~ ~
+
+Design Patterns
+To The Rescue
+
+Origins of patterns 
+Architect Christopher Alexander 
+Book: A Pattern Language. Towns. Buildings. Construction
+
+“...each pattern represents our current
+best guess as to what arrangement of
+the physical environment will work to
+solve the problem presented.”
+Christopher Alexander 
+
+“The empirical questions center on
+the problem
+—does it occur and is it felt in
+the way we have described it?”
+Christopher Alexander 
+
+“and the solution
+—does the arrangement we
+propose in fact resolve the problem?”
+Christopher Alexander 
+
+~ ~ ~ ~ ~
+
+Design Patterns
+
+|       Problems       |        Solutions         |
+- On and Off Traffic    - Cloverleaf Interchanges  
+for Highways
+- Pedestrian Traffic        - Sidewalks…
+- Entry and Exit for     - Revolving Doors
+Public Buildings
+
+Design Patterns
+
+|     Problems        |        Solutions       |
+- Designing Service      - Module Pattern
+Layers
+- Overly Complicated     - Façade Pattern
+Object Interfaces
+- Visibility Into        - Observer Pattern
+State Changes
+
+~ ~ ~ ~ ~
+
+Book from the Gang of Four
+
+Design Patterns. Elements of Reusable Object-Oriented Software
+
+Erich Gamma
+Richard Helm
+John Vlissides
+
+~ ~ ~ ~ ~
+
+So, What Constitutes a Pattern?
+
+> It solves a problem
+> It is a proven concept
+> The solution is not obvious
+> It described a relationship
+> It has a significant human component
+
+Why Bother With Patterns?
+- Why solve it again? - Common Vocabulary
+
+Types Of Patterns
+
+~ ~ ~ ~ ~
+
+Creational
+1) Constructor
+2) Module
+3) Factory
+4) Singleton
+
+Structural
+1) Decorator
+2) Façade
+3) Flyweight
+
+Behavioral
+1) Command
+2) Mediator
+3) Observer
+
+Rules of the Road
+
+* Objects In JavaScript
+* Each Pattern Stands On Its Own
+* Describe Each Pattern
+* Implement in Node
+* Implement in Angular
+
+*/
