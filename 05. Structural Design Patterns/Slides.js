e9352abc381abbfb22a1b47dbec9c820317aefe5
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-19 15:44:00 +0300

    05. Structural Design Patterns is finished!

--- 05. Structural Design Patterns/Slides.js
+++ 05. Structural Design Patterns/Slides.js
@@ -1,2 +1,51 @@
 /* Structural Design Patterns
 
+Concerned with how objects are made up and simplify relationships between objects
+
+Structural Patterns
+> Deal with the relationship of objects
+> Extend functionality
+> Simplify functionality
+
+Decorator Pattern
+- Used to add new functionality to an existing object,
+without being obtrusive. 
+
+
++ More complete inheritance
++ Wraps an object
++ Protects existing objects
++ Allows extended functionality
+
+
+
+Façade Pattern
+Used to provide a simplified interface to a complicated
+system
+
+Think about the front of a building
+Facade hides the chaos from us
+Simplifies the interface
+Think JQuery
+
+
+
+
+Flyweight Pattern
+Conserves memory by sharing portions of an object
+between objects. 
+
+Our tasks had lots of non unique data
+Flyweight shares data across objects
+Results in a smaller memory footprint
+Like a boxer --- a flyweight….
+But only if you have large numbers of
+objects
+
+Summary
+
+Structural Design Patterns
+Decorator Pattern
+Façade Pattern
+Flyweight Pattern
+
